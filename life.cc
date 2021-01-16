04eebb02549c61dfb4f94ca28888abbb6c638403
zyziuMarchewka <witek.kedra@gmail.com>
2021-01-16 11:44:13 +0100

    working simpler, brutal form

--- life.cc
+++ life.cc
@@ -4,36 +4,6 @@ using namespace std;
 
 const int BUFFOR = 2;
 
-struct Cartograph {
-  int nmost;
-  int smost;
-  int wmost;
-  int emost;
-
-  Cartograph(int n, int m) {
-    nmost = n;
-    smost = 0;
-
-    wmost = m;
-    emost = 0;
-  }
-
-  void reset(int n, int m){
-    nmost = n;
-    smost = 0;
-
-    wmost = m;
-    emost = 0;
-  }
-
-  void update(int i, int j, int difY, int difX){
-    if(i<(nmost+difY)) nmost=i;
-    if(i>(smost+difY)) smost=i;
-    if(j<(wmost+difX)) wmost=j;
-    if(j>(emost+difX)) emost=j;
-  }
-};
-
 int main() {
 
   int C;
@@ -44,14 +14,12 @@ int main() {
     vector <vector <bool> > map(n+2*BUFFOR, vector <bool> (m+2*BUFFOR, false));
     int suma = 0;
 
-    Cartograph cartograph(n+2*BUFFOR, m+2*BUFFOR);
     for(int i=BUFFOR; i<=n+1; i++){
       string input;
       cin >> input;
       for(int j=BUFFOR; j<=m+1; j++){
         if(input[j-BUFFOR] == 'o'){
           map[i][j] = true;
-          cartograph.update(i, j, 0, 0);
         }
       }
     }
@@ -60,17 +28,9 @@ int main() {
 
     int T;
     cin >> T;
-    for(int t=0; t<T; t++){
-      int difY = (cartograph.nmost-2)*(-1);
-      int difX = (cartograph.wmost-2)*(-1);
-
-      int nNew = cartograph.smost-cartograph.nmost+1+2*BUFFOR;
-      int mNew = cartograph.emost-cartograph.wmost+1+2*BUFFOR;
-
-      cartograph.reset(nNew, mNew);
-
-      vector <vector <bool> > newMap(nNew, vector <bool> (mNew, false));
-
+    //for(int t=0; t<T; t++){
+    while(T--){
+      vector <vector <bool> > newMap(n+2, vector <bool> (m+2, false));
       int count = 0;
 
       for(int i=1; i<(n-1); i++){
@@ -86,25 +46,28 @@ int main() {
           neighbors -= map[i][j];
 
           if(neighbors == 3 || (map[i][j] && neighbors==2)){
-            newMap[i+difY][j+difX] = true;
-            cartograph.update(i, j, difY, difX);
+            newMap[i+1][j+1] = true;
             count ++;
           }
         }
       }
 
-      n = nNew;
-      m = mNew;
+      n += 2;
+      m += 2;
       map = newMap;
-      // DEBUG CHECK //
-      for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-          if(map[i][j]) cerr << 'o';
-          else cerr << '-';
-        }
-        cerr << endl;
-      } ///////////////
+      // // DEBUG CHECK //
+      // for(int i=0; i<n; i++){
+      //   for(int j=0; j<m; j++){
+      //     if(map[i][j]) cerr << 'o';
+      //     else cerr << '-';
+      //   }
+      //   cerr << endl;
+      // } ///////////////
       cout << count << "\n";
+      if(count == 0){
+        //for(int i=t; i<T; i++) cout << 0 << "\n";
+        while(T--) cout << 0 << "\n";
+      }
     }
   } 
 }
